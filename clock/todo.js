2e3776476177de89fcd2e7f21762c08c948ce3a2
SEONGBEOM-KIM <59245627+SEONGBEOM-KIM@users.noreply.github.com>
2020-02-01 22:37:49 +0900

    Making ToDoList

---
+++ clock/todo.js
@@ -0,0 +1,58 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoForm.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "X";
+  const span = document.createElement("span");
+  span.innerText = text;
+  const newId = toDos.length + 1;
+  li.appendChild(delBtn);
+  li.appendChild(span);
+  li.id = newId;
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+// ul, li, button, span을 이용해서 리스트를 화면에 띄우고 localStorage에 저장함.
+// 저장은 되어 있지만 refresh하면 저장만 되어있을 뿐 화면에는 사라짐.
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos); //string ---> objetc
+    parsedToDos.forEach(function(toDo) {
+      paintToDo(toDo.text); //localStorage에 값이 있을 때(!== null) 항상 화면에 띄우기
+    });
+  }
+}
+
+//toDo is object like {text : eating, id : 1}, so toDo.text means "eating"!
+
+function init() {
+  loadToDos();
+  toDoForm.addEventListener("submit", handleSubmit);
+}
+init();
